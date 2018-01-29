36ba7baf3564a215d16b5eaf8c81209106c97766
Emily Mame Ford <emilyford352@gmail.com>
2018-01-28 21:54:49 -0500

    editing image, etc

--- em-resume/src/Components/Header.js
+++ em-resume/src/Components/Header.js
@@ -4,19 +4,25 @@ class Header extends Component {
   render() {
     return (
       <div className="Header">
-        <header className="App-header">
+        <header className="App-header text-center">
           <h1 className="App-title">Emily Mame Ford</h1>
           <p> Acton, MA </p>
-          <div className="row text-center">
-              <a title="GitHub" href="https://github.com/emilyford352">
-                  <i className="fa fa-github col-sm-1"></i>
-              </a>
-              <a title="LinkedIn" href="https://www.linkedin.com/in/emily-ford-2385419b/">
-                  <i className="fa fa-linkedin col-sm-1"></i>
-              </a>
-              <a title="Paper Resume" href="./resume.pdf">
-                <i className="fa fa-file-text-o col-sm-1"></i>
-              </a>
+          <div >
+              <div className="meta-link">
+                  <a title="GitHub" href="https://github.com/emilyford352">
+                      <i className="fa fa-github"></i>
+                  </a>
+              </div>
+              <div className="meta-link">
+                  <a title="LinkedIn" href="https://www.linkedin.com/in/emily-ford-2385419b/">
+                      <i className="fa fa-linkedin"></i>
+                  </a>
+              </div>
+              <div className="meta-link">
+                  <a title="Paper Resume" href="./resume.pdf">
+                    <i className="fa fa-file-text-o"></i>
+                  </a>
+              </div>
           </div>
         </header>
       </div>
--- em-resume/src/Components/ImagePane.js
+++ em-resume/src/Components/ImagePane.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 //import logo from './eevee.png';
-import profileImage from '../Images/emily.jpg';
+import profileImage from '../Images/emily.png';
 
 class ImagePane extends Component {
   render() {
     return (
       <div className="ImagePane">
-        <div className="row">
-            <div className="col-md-4 col-centered profile-image-container">
+          <div className="row">
+              <div className="col-centered">
                 <img src={profileImage} className="profile-image" alt="Emily" />
-            </div>
-        </div>
+              </div>
+          </div>
       </div>
     );
   }
