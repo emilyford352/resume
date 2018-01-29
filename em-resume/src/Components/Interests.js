1dba8290b04500133ff0c0af05dba83e6251bef8
Emily Mame Ford <emilyford352@gmail.com>
2018-01-28 22:18:29 -0500

    setting up component skeleton

---
+++ em-resume/src/Components/Interests.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Interests extends Component {
+  render() {
+    return (
+      <div className="Interests">
+      </div>
+    );
+  }
+}
+
+export default Interests;
---
+++ em-resume/src/Components/WorkExperience.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class WorkExperience extends Component {
+  render() {
+    return (
+      <div className="WorkExperience">
+      </div>
+    );
+  }
+}
+
+export default WorkExperience;
